0f498b215538402e25c82a21c1564269641d112a
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-03-27 15:53:00 +0200

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen bg-[#111] text-color">
+      <h1>AI service page</h1>
+    </main>
+  );
+}
